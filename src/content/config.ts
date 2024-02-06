94a28d1e3aa4ca5995cf10723a7a660ac6c71993
Kevhec <kevhec.dev@gmail.com>
2024-02-06 00:08:58 -0500

    [CHANGE] Project Header outside mdx file, png to webp

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,9 @@ const projectCollection = defineCollection({
     title: z.string(),
     projectName: z.string(),
     excerpt: z.string(),
+    description: z.string(),
+    wasForClient: z.boolean(),
+    stack: z.array(z.string()),
     mockup: image().refine((img) => img.width >= 375, {
       message: "Cover image must be at least 1080 pixels wide!",
     }),
